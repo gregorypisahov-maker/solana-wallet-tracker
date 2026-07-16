e912e317d7ddf77ab73266c0d52bc1565f309114
gregorypisahov-maker <gregorypisahov@gmail.com>
2026-07-16 11:20:17 +0300

    Fix CommonJS bootstrap startup

--- worker/monitorBootstrap.ts
+++ worker/monitorBootstrap.ts
@@ -1,5 +1,12 @@
 import "dotenv/config";
 import { startWalletDiscoveryScheduler } from "./walletDiscovery";
 
-startWalletDiscoveryScheduler();
-await import("./monitor");
+async function main(): Promise<void> {
+  startWalletDiscoveryScheduler();
+  await import("./monitor");
+}
+
+main().catch((error) => {
+  console.error("[monitor-bootstrap] Fatal startup error:", error);
+  process.exit(1);
+});
