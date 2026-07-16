aa6d0f822fe36b9f3d2a0235c0538fbc66386fc3
gregorypisahov-maker <gregorypisahov@gmail.com>
2026-07-16 11:56:21 +0300

    Start automatic wallet intelligence scheduler

--- worker/monitorBootstrap.ts
+++ worker/monitorBootstrap.ts
@@ -1,9 +1,11 @@
 import "dotenv/config";
 import { startAuditedWalletDiscoveryScheduler } from "./walletDiscoveryAudit";
+import { startWalletIntelligenceScheduler } from "./walletIntelligence";
 import { startAdaptiveStrategyScheduler } from "../paper-trader/adaptiveStrategy";
 
 async function bootstrap(): Promise<void> {
   startAuditedWalletDiscoveryScheduler();
+  startWalletIntelligenceScheduler();
   startAdaptiveStrategyScheduler();
   await import("./monitor");
 }
