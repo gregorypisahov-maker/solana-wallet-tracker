82a8f18e9c44bae4af9f4f9b382b25bb889c84fb
gregorypisahov-maker <gregorypisahov@gmail.com>
2026-07-16 12:21:06 +0300

    Start shadow strategy with monitor

--- worker/monitorBootstrap.ts
+++ worker/monitorBootstrap.ts
@@ -1,12 +1,14 @@
 import "dotenv/config";
 import { startAuditedWalletDiscoveryScheduler } from "./walletDiscoveryAudit";
 import { startWalletIntelligenceScheduler } from "./walletIntelligence";
+import { startShadowStrategyScheduler } from "./shadowStrategyScheduler";
 import { startAdaptiveStrategyScheduler } from "../paper-trader/adaptiveStrategy";
 
 async function bootstrap(): Promise<void> {
   startAuditedWalletDiscoveryScheduler();
   startWalletIntelligenceScheduler();
   startAdaptiveStrategyScheduler();
+  startShadowStrategyScheduler();
   await import("./monitor");
 }
 
