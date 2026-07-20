cd30ffdf62aa69f89d1209b5c5fe9c040b043035
gregorypisahov-maker <gregorypisahov@gmail.com>
2026-07-21 01:29:08 +0300

    scalper shadow: start independent scheduler

--- worker/monitorBootstrap.ts
+++ worker/monitorBootstrap.ts
@@ -5,6 +5,7 @@ import { startShadowStrategyScheduler } from "./shadowStrategyScheduler";
 import { startAdaptiveStrategyScheduler } from "../paper-trader/adaptiveStrategy";
 import { startPaperAutoResumeScheduler } from "./paperAutoResume";
 import { startMomentumScalperScheduler } from "../paper-trader/momentumScalper";
+import { startScalperShadowScheduler } from "../paper-trader/scalperShadow";
 import { startLiveReadinessScheduler } from "../paper-trader/liveReadiness";
 
 const WALLET_DISCOVERY_SERVICE = "Wallet Discovery & Monitor";
@@ -25,22 +26,17 @@ async function bootstrap(): Promise<void> {
     startAuditedWalletDiscoveryScheduler();
     startWalletIntelligenceScheduler();
   } else {
-    console.log(
-      `[monitor-bootstrap] wallet management disabled in ${process.env.RAILWAY_SERVICE_NAME}; ` +
-        `owned by ${WALLET_DISCOVERY_SERVICE}`
-    );
+    console.log(`[monitor-bootstrap] wallet management disabled in ${process.env.RAILWAY_SERVICE_NAME}; owned by ${WALLET_DISCOVERY_SERVICE}`);
   }
 
   if (shouldStartTradingStrategies()) {
     startAdaptiveStrategyScheduler();
     startShadowStrategyScheduler();
     startMomentumScalperScheduler();
+    startScalperShadowScheduler();
     startLiveReadinessScheduler();
   } else {
-    console.log(
-      `[monitor-bootstrap] trading schedulers disabled in ${process.env.RAILWAY_SERVICE_NAME}; ` +
-        `owned by ${TRADING_ENGINE_SERVICE}`
-    );
+    console.log(`[monitor-bootstrap] trading schedulers disabled in ${process.env.RAILWAY_SERVICE_NAME}; owned by ${TRADING_ENGINE_SERVICE}`);
   }
 
   startPaperAutoResumeScheduler();
