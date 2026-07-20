22ff19b6b9ce8faf3450fcd501bae4f6de196df2
gregorypisahov-maker <gregorypisahov@gmail.com>
2026-07-20 23:09:11 +0300

    regular: start v2 forward-test cohort

--- paper-trader/strategyVersion.ts
+++ paper-trader/strategyVersion.ts
@@ -1,2 +1,2 @@
-export const REGULAR_STRATEGY_VERSION = "regular_hybrid_v1_2026_07_18";
+export const REGULAR_STRATEGY_VERSION = "regular_hybrid_v2_2026_07_20";
 export const SHADOW_STRATEGY_VERSION = "shadow_strategy_v1_2026_07_20";
