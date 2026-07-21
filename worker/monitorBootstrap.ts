c1bacbce0727d85f39ee3f9d2453c4dd6733e6eb
gregorypisahov-maker <gregorypisahov@gmail.com>
2026-07-21 08:10:54 +0300

    remove broken scalper shadow runtime

--- worker/monitorBootstrap.ts
+++ worker/monitorBootstrap.ts
@@ -5,7 +5,6 @@ import { startShadowStrategyScheduler } from "./shadowStrategyScheduler";
 import { startAdaptiveStrategyScheduler } from "../paper-trader/adaptiveStrategy";
 import { startPaperAutoResumeScheduler } from "./paperAutoResume";
 import { startMomentumScalperScheduler } from "../paper-trader/momentumScalper";
-import { startScalperShadowScheduler } from "../paper-trader/scalperShadow";
 import { startLiveReadinessScheduler } from "../paper-trader/liveReadiness";
 
 const WALLET_DISCOVERY_SERVICE = "Wallet Discovery & Monitor";
@@ -33,7 +32,6 @@ async function bootstrap(): Promise<void> {
     startAdaptiveStrategyScheduler();
     startShadowStrategyScheduler();
     startMomentumScalperScheduler();
-    startScalperShadowScheduler();
     startLiveReadinessScheduler();
   } else {
     console.log(`[monitor-bootstrap] trading schedulers disabled in ${process.env.RAILWAY_SERVICE_NAME}; owned by ${TRADING_ENGINE_SERVICE}`);
