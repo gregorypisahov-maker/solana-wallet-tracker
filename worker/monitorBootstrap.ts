232d25c548497fda6d75fbcf6c3b58dc9e604ee9
gregorypisahov-maker <gregorypisahov@gmail.com>
2026-07-21 09:06:21 +0300

    tiered: start entry shadow scheduler

--- worker/monitorBootstrap.ts
+++ worker/monitorBootstrap.ts
@@ -6,6 +6,7 @@ import { startAdaptiveStrategyScheduler } from "../paper-trader/adaptiveStrategy
 import { startPaperAutoResumeScheduler } from "./paperAutoResume";
 import { startMomentumScalperScheduler } from "../paper-trader/momentumScalper";
 import { startScalperShadowScheduler } from "../paper-trader/scalperShadow";
+import { startTieredEntryShadowScheduler } from "../paper-trader/tieredEntryShadow";
 import { startLiveReadinessScheduler } from "../paper-trader/liveReadiness";
 
 const WALLET_DISCOVERY_SERVICE = "Wallet Discovery & Monitor";
@@ -35,6 +36,7 @@ async function bootstrap(): Promise<void> {
     startShadowStrategyScheduler();
     startMomentumScalperScheduler();
     startScalperShadowScheduler();
+    startTieredEntryShadowScheduler();
     startLiveReadinessScheduler();
   } else {
     console.log(`[monitor-bootstrap] trading schedulers disabled in ${process.env.RAILWAY_SERVICE_NAME}; owned by ${TRADING_ENGINE_SERVICE}`);
