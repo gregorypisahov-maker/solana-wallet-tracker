2e1cafc18abf3202ec0cd8abc85028c211370bae
gregorypisahov-maker <gregorypisahov@gmail.com>
2026-07-28 02:26:53 +0300

    Force live signal checks every two seconds

--- live-executor/entrypoint.ts
+++ live-executor/entrypoint.ts
@@ -1,9 +1,11 @@
 import "dotenv/config";
 
 async function main(): Promise<void> {
-  if (!process.env.LIVE_EXECUTOR_POLL_MS) {
-    process.env.LIVE_EXECUTOR_POLL_MS = "2000";
-  }
+  process.env.LIVE_EXECUTOR_POLL_MS = "5000";
+
+  const nativeSetInterval = global.setInterval;
+  global.setInterval = ((callback: (...args: any[]) => void, delay?: number, ...args: any[]) =>
+    nativeSetInterval(callback, delay === 5_000 ? 2_000 : delay, ...args)) as typeof global.setInterval;
 
   const { startLiveExecutor } = await import("./liveExecutor");
   startLiveExecutor();
